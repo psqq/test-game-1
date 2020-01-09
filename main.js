bb3e1a08309bf51e10c0e883892e44a7002a67d5
Sergey Pestov <pestov-sa@yandex.ru>
2020-01-09 05:54:49 +0000

    test

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const rot = ROT;
 const display = new rot.Display({
   width: 30,
   height: 30,
+  forceSquareRatio: true,
 });
 
 document.body.appendChild(display.getContainer());
 
-display.draw(1, 1, '@', 'white', 'black');
+display.draw(3, 1, '@', 'white', 'black');
